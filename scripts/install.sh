2ff4c4d974d3d6836c9da051480ae6291b1139cc
davidecotten <davidecotten@gmail.com>
2018-08-07 00:12:32 +0000

    Fixed install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,7 +4,7 @@ apt-get install python-dev libbluetooth-dev libcap2-bin ffmpeg
 setcap 'cap_net_raw,cap_net_admin+eip' $(readlink -f $(which python))
 
 cd /home/pi/waifu-badge
-pip3 install -r requirements.txt
+sudo pip3 install -r requirements.txt
 
 sudo cp /home/pi/waifu-badge/scripts/rc.local /etc/rc.local
 
